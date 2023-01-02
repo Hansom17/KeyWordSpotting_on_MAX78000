c847b7ed8987f2fbda6cfd2af8e8a567bac0b787
hanph1 <philipp.hansel@stadlerrail.com>
2023-01-02 12:45:07 +0100

    test on chip leftandright

---
+++ leftandright/conv1d/kws20v2/sampleoutput.h
@@ -0,0 +1,5 @@
+// This file was @generated automatically
+
+#define SAMPLE_OUTPUT { \
+  0x50402000, 0xffffffff, 0x00000003, 0x00000a02, 0x00000d70, 0xffffdebc, 0x00000000 \
+}
---
+++ leftandright/optimiced/kwscnn/sampleoutput.h
@@ -0,0 +1,5 @@
+// This file was @generated automatically
+
+#define SAMPLE_OUTPUT { \
+  0x50404000, 0x00ffffff, 0x00000001, 0x0000f000, 0x00000000 \
+}
